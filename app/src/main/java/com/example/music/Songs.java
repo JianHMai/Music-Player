cd98ff07b282b3b4cb989c018361f4fe7001c1cf
JianHMai <42229566+JianHMai@users.noreply.github.com>
2019-09-29 23:34:02 -0400

    first commit

---
+++ app/src/main/java/com/example/music/Songs.java
@@ -0,0 +1,26 @@
+package com.example.music;
+
+public class Songs {
+    private String title;
+    private String artist;
+    private String path;
+
+    public String getPath(){
+        return path;
+    }
+    public void setPath(String path){
+        this.path=path;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public String getArtist(){
+        return artist;
+    }
+    public void setArtist(String artist){
+        this.artist=artist;
+    }
+}
